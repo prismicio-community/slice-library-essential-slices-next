226f6ab7175d7de54036ccee01d6ea019210cef0
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-12-17 10:29:17 -1000

    refactor: use cx helper over clsx

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,6 +1,7 @@
 import { PrismicLink, PrismicLinkProps } from "@prismicio/react";
 import * as prismicH from "@prismicio/helpers";
-import clsx from "clsx";
+
+import { cx } from "../cx";
 
 export type ButtonLinkProps<
 	LinkResolverFunction extends prismicH.LinkResolverFunction = prismicH.LinkResolverFunction,
@@ -18,7 +19,7 @@ export function ButtonLink<
 }: ButtonLinkProps<LinkResolverFunction>) {
 	return (
 		<PrismicLink
-			className={clsx("es-button", `es-button--${variant}`, className)}
+			className={cx("es-button", `es-button--${variant}`, className)}
 			{...restProps}
 		>
 			{children}
